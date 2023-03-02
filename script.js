61cf8ccadd6888d412146b0fe3cda4b10a6f659b
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-03-02 18:01:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,43 +5,43 @@ const navCart = document.getElementById("nav-cart-icon");
 const navUser = document.getElementById("nav-user-icon");
 
 navSearch.parentNode.addEventListener("mouseover", function() {
-    navSearch.src = "/icons/search-after.png";
+    navSearch.src = "./icons/search-after.png";
 });
 
 navSearch.parentNode.addEventListener("mouseout", function() {
-    navSearch.src = "/icons/search.png";
+    navSearch.src = "./icons/search.png";
 });
 
 navOffer.parentNode.addEventListener("mouseover", function() {
-    navOffer.src = "/icons/discount-after.png";
+    navOffer.src = "./icons/discount-after.png";
 });
 
 navOffer.parentNode.addEventListener("mouseout", function() {
-    navOffer.src = "/icons/discount.png";
+    navOffer.src = "./icons/discount.png";
 });
 
 navHelp.parentNode.addEventListener("mouseover", function() {
-    navHelp.src = "/icons/help-after.png";
+    navHelp.src = "./icons/help-after.png";
 });
 
 navHelp.parentNode.addEventListener("mouseout", function() {
-    navHelp.src = "/icons/help.png";
+    navHelp.src = "./icons/help.png";
 });
 
 navCart.parentNode.addEventListener("mouseover", function() {
-    navCart.src = "/icons/cart-after.png";
+    navCart.src = "./icons/cart-after.png";
 });
 
 navCart.parentNode.addEventListener("mouseout", function() {
-    navCart.src = "/icons/cart.png";
+    navCart.src = "./icons/cart.png";
 });
 
 navUser.parentNode.addEventListener("mouseover", function() {
-    navUser.src = "/icons/user-after.png";
+    navUser.src = "./icons/user-after.png";
 });
 
 navUser.parentNode.addEventListener("mouseout", function() {
-    navUser.src = "/icons/user.png";
+    navUser.src = "./icons/user.png";
 });
 
 
